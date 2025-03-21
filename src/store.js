76c8ff152214aa2a3daf76767c99b904a6ec58f4
Aleksandr Vysotski <sasha.vysocki@gmail.com>
2025-03-21 23:38:45 +0300

    Реализовать стор и редьюсеры

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { thunk } from 'redux-thunk';
+import { membersReducer, favoritesReducer } from './reducer';
+
+const reducer = combineReducers({
+	members: membersReducer,
+	favorites: favoritesReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
