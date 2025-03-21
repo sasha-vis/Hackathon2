e36b4df7f69f2d7fdbeb861872de1b1a9cf6a177
Andrey <rasgis@yandex.ru>
2025-03-21 21:49:20 +0300

    initial page MemberPage and MemberProfile

--- src/pages/memberPage/MemberPage.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './MemberPage.module.css';
-
-export const MemberPage = () => {
-	return <div className={styles.MemberPage}>MemberPage</div>;
-};
---
+++ src/pages/memberPage/MemberPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MemberProfile } from '../../components/memberProfile/MemberProfile';
+import styles from './MemberPage.module.css';
+
+export const MemberPage: React.FC = () => {
+    const memberData = {
+      firstName: "Андрей",
+      lastName: "Лазурко",
+      age: 36,
+      about: "Опытный frontend-разработчик с страстью к созданию удобных пользовательских интерфейсов.",
+      photo: "/img/rasgis.jpg",
+      defaultImage: "/img/rasgis.jpg",
+      socialLinks: {
+        github: "https://github.com/rasgis",
+        vk: "https://vk.com/rasgis",
+        telegram: "https://t.me/zovrasgis"
+      },
+      projectContribution: "Разработал компоненты пользовательского интерфейса и настроил маршрутизацию в приложении."
+    };
+
+    return (
+      <div className={styles.container}>
+        <MemberProfile {...memberData} />
+      </div>
+    );
+  };
