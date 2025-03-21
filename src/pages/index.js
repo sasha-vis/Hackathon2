46ad5c19d14de59fb278e9add0bb54485fac7ce3
Aleksandr Vysotski <sasha.vysocki@gmail.com>
2025-03-21 19:57:23 +0300

    Реализовать роутинг

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export * from './mainPage/MainPage';
+export * from './memberPage/MemberPage';
+export * from './favoritesPage/FavoritesPage';
+export * from './notFoundPage/NotFoundPage';
---
+++ src/pages/notFoundPage/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+import styles from './NotFoundPage.module.css';
+
+export const NotFoundPage = () => {
+	return <div className={styles.NotFoundPage}>NotFoundPage</div>;
+};
